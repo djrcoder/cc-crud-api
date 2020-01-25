d2e11a2a2380d11a996b781b0ecc1d47a91b3d8b
djrcoder <djrcode@gmail.com>
2020-01-25 16:43:02 +0900

    Added database connection file.

---
+++ server/database/knex.js
@@ -0,0 +1,17 @@
+const envirnoment = process.env.NODE_ENV || + 'development';
+// Dev will use development connection, else production connection.
+
+const config = require("../knexfile");
+
+const envirnomentConfig = config[envirnoment];
+
+const knex = require("knex");
+
+// All the above information will connect to the database
+
+const connection = knex(envirnomentConfig);
+
+module.exports = connection;
+
+
+
