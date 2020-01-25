2c6fe00692bc0fc25d3fe547a68ed74306ebf3a4
djrcoder <djrcode@gmail.com>
2020-01-25 17:16:51 +0900

    First .get() retrieves all data in JSON form.

--- server/api/bikeapi.js
+++ server/api/bikeapi.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const queries = require("../database/queries")  // use knex queries file
 
 router.get("/", (request, response) => {
-    response.json({
-        message: "Hello!"
-    });
+    queries.getEverything().then(bikes => {
+        response.json({
+            bikes
+        })
+    })
+
+    // response.json({
+    //     message: "Hello!"
+    // });
 });
 
 module.exports = router;
+
+
+
+// response.json({
+//     message: "Hello!"
+// });
