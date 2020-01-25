b6d91f43ece168dd5d8311998e8a6cfb792c8877
djrcoder <djrcode@gmail.com>
2020-01-25 17:56:40 +0900

    Initial.

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (request, response) => response.send('I am Node!'))
-
-app.listen(port, () => console.log(`My blog is listening on port ${port}!`))
