2c9adf4fd9fdfd8ad82f791cf56de720044cde3c
djrcoder <djrcode@gmail.com>
2020-01-25 23:33:13 +0900

    Initial commits.

---
+++ server/activityData.js
@@ -0,0 +1,68 @@
+// activity_id | time_date | distance | total_time
+
+module.exports = [
+    {
+        activity_id: 3027,
+        time_date: "2020-01-20 09:54:32",
+        distance: 14,
+        total_time: 2947
+    },
+
+    {
+        activity_id: 3028,
+        time_date: "2020-01-20 22:53:24",
+        distance: 15,
+        total_time: 3136
+    },
+
+    {
+        activity_id: 3029,
+        time_date: "2020-01-21 10:00:17",
+        distance: 9,
+        total_time: 2989
+    },
+
+    {
+        activity_id: 3030,
+        time_date: "2020-01-21 22:50:08",
+        distance: 8,
+        total_time: 3031
+    },
+
+    {
+        activity_id: 3031,
+        time_date: "2020-01-22 09:28:04",
+        distance: 16,
+        total_time: 3683
+    },
+
+    {
+        activity_id: 3032,
+        time_date: "2020-01-22 22:52:48",
+        distance: 10,
+        total_time: 3059
+    },
+
+    {
+        activity_id: 3033,
+        time_date: "2020-01-23 10:15:46",
+        distance: 16,
+        total_time: 4281
+    },
+
+
+    {
+        activity_id: 3034,
+        time_date: "2020-01-23 22:58:12",
+        distance: 15,
+        total_time: 2989
+    },
+
+    {
+        activity_id: 3035,
+        time_date: "2020-01-24 13:12:53",
+        distance: 16,
+        total_time: 4364
+    },
+
+]
---
+++ server/logData.js
@@ -0,0 +1,60 @@
+// id | activity_id | type_of_ride | activity_type
+
+
+module.exports = [
+    {
+        activity_id: 3027,
+        type_of_ride: "Evening Ride",
+        activity_type: "Ride"
+    },
+
+    {
+        activity_id: 3028,
+        type_of_ride: "Morning Ride",
+        activity_type: "Ride"
+    },
+
+    {
+        activity_id: 3029,
+        type_of_ride: "Morning Run",
+        activity_type: "Run"
+    },
+
+    {
+        activity_id: 3030,
+        type_of_ride: "Evening Run",
+        activity_type: "Run"
+    },
+
+    {
+        activity_id: 3031,
+        type_of_ride: "Evening Ride",
+        activity_type: "Ride"
+    },
+
+    {
+        activity_id: 3032,
+        type_of_ride: "Evening Run",
+        activity_type: "Run"
+    },
+
+    {
+        activity_id: 3033,
+        type_of_ride: "Evening Ride",
+        activity_type: "Ride"
+    },
+
+
+    {
+        activity_id: 3034,
+        type_of_ride: "Morning Ride",
+        activity_type: "Ride"
+    },
+
+    {
+        activity_id: 3035,
+        type_of_ride: "Night Ride",
+        activity_type: "Ride"
+    },
+
+]
