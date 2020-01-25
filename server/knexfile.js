79f62c1768b3e0773f8808c65fe3ae80f3a7545c
djrcoder <djrcode@gmail.com>
2020-01-25 19:44:09 +0900

    Added test connection.

--- server/knexfile.js
+++ server/knexfile.js
@@ -5,15 +5,11 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/dancrud'
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/dancrud'
   }
+
 };
-
-
-// test: {
-//   client: 'pg',
-//   connection: 'postgres://localhost/dancrud'
-// },
-// production: {
-//   client: 'pg',
-//   connection: process.env.DATABASE_URL
-// }
